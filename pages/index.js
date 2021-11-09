3ed2b4b72d58def822af039e1eedbfe7d5444aea
Karina Dalileh <karinad89@gmail.com>
2021-11-09 11:30:22 +0100

    connection to firebase database insted of hard coded dummy-data

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,26 @@
-import { getFeaturedEvents } from '../dummy-data';
+import { getFeaturedEvents } from '../helpers/api-util';
 import EventList from '../components/events/event-list';
 
-function HomePage() {
+// this will be executed after getStaticProps
+// to get to events, we need to pass (props) and then use as props.events
+function HomePage(props) {
   const featuredEvents = getFeaturedEvents();
   return (
     <div>
       <h1>Featured events</h1>
-      <EventList items={featuredEvents} />
+      <EventList items={props.events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
 }
 
 export default HomePage;
